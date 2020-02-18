8572ae1e5a88bf33f8ee251cd73dac3546911c34
Caleb <calebljeffrey@gmail.com>
2020-02-17 23:50:51 -0600

    Added the program

---
+++ calculator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int value = 0;
+	for (int i = 1; i < argc; i++)
+	{
+		value+=atoi(argv[i]);
+	}
+	cout << value;
+}
